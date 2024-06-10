24f27dce2a8911a77fa3365e974d536dc0125de5
medr0k <143861386+medr0k@users.noreply.github.com>
2024-06-09 18:47:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ bubbleImage.onerror = (error) => {
 
 const bubbles = [];
 const bubbleCount = 20; // Reduced bubble count
-const bubbleSize = 80; // Smaller bubble size
+const bubbleSize = 70; // Smaller bubble size
 
 class Bubble {
     constructor(x, y) {
